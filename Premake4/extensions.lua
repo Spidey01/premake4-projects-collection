3ba2717c2c62c34f63dacef0e69fa3687113e058
Terry Matthew Poulin <BigBoss1964@gmail.com>
2010-11-25 23:32:55 +0000

    headers() now uses prebuildcommands instead of premake4 time lua calls
    I've also noticed that a number of premake4's path.*() functions get
    rather pissy if you have a $ in certain spots. So the code in headers()
    does a ugly %$ escape and %$ to $ conversion. %$ is the escape of $ in
    Lua string patterns.

--- Premake4/extensions.lua
+++ Premake4/extensions.lua
@@ -112,17 +112,17 @@ if not premake4_extensions_lua then
     function headers(headers)
         local p = project()
 
+        -- path.join() has a bug in it that causes it to fail if the second
+        -- argument starts with a $, and that may happen here: so use this func
+        -- instead.
         --
-        -- This needs to run for each configuration because (for consistency)
-        -- headers get installed into a configuration specific path along with
-        -- the associated binaries. BREAK JUDICIOUSLY!
-        --
-        local cfgs = configurations()
-        for c=1, #cfgs do
-            local cfg = cfgs[c]
-            incdir = path.join(DIST_DIR, path.join(cfg, "include"))
+        local function join(a, b)
+            return a .. "/" .. b
+        end
 
-            if _ACTION == "clean" then
+        if _ACTION == "clean" then
+            local function clean(cfg)
+                local incdir = join(DIST_DIR, join(cfg, "include"))
                 local t = {}
                 local function rm(f)
                     if os.isfile(f) then os.remove(f)
@@ -131,26 +131,83 @@ if not premake4_extensions_lua then
                     end
                 end
                 for i=1, #headers do
-                    rm(path.join(incdir, headers[i]))
-                end
-            elseif _ACTION then
-                if not os.isdir(incdir) then os.mkdir(incdir) end
-                local t = {}
-                for i=1, #headers do
-                    header = path.join(sourcedir(), headers[i])
-                    dest = path.join(incdir, path.getname(header))
-                    if os.isfile(header) then
-                        os.copyfile(header, dest)
-                    elseif os.isdir(header) then
-                        error("headers() doesn't do directories yet, sorry chap!")
-                    else
-                        error("You can't use headers() to copy a type of " ..
-                              type(header) .. " with value " .. header ..
-                              " If that makes no sense, you forgot to " ..
-                              "unpack the source code!")
-                    end
+                    rm(join(incdir, headers[i]))
                 end
             end
+            --
+            -- This needs to run for each configuration because (for
+            -- consistency) headers get installed into a configuration specific
+            -- path along with the associated binaries, and premake4 clean
+            -- works like this also. BREAK JUDICIOUSLY!
+            --
+            map(clean, configurations())
+        elseif _ACTION then
+            --
+            -- setup pre-build-commands to get the job done.
+            --
+            -- XXX this needs to be done for each _ACTION we support.
+            --     and we need to create the include directories for
+            --     each configuration.
+            --
+            map(function (cfg)
+                    os.mkdir(join(DIST_DIR, join(cfg, "include")))
+                end, configurations())
+
+            -- This must be set to a string suitable for use as
+            -- string.format(copycmd, headerfile, foldername) to be passed on
+            -- to prebuildcommands()
+            --
+            local copycmd = ""
+
+            -- This must be set to the directory in DIST_DIR that headers are
+            -- placed in for the current configuration, as accessible from the
+            -- _ACTIONs prebuildcommands. I.e. allowing you to use gmake/vs20*
+            -- macros and the like.
+            --
+            --
+            -- $ must be entered as %$ to avoid a bug in premake.
+            -- It is unescaped back to $ later in this function.
+            --
+            local incdir = ""
+
+            if _ACTION == "gmake" then
+                copycmd = "cp -R \"%s\" \"%s\""
+
+                -- OBJDIR will be, e.g. ReleaseShared/<projectname>
+                incdir = join("`dirname $(OBJDIR)`", "include")
+            elseif string.match(_ACTION, "vs20*") then
+                copycmd = "COPY \"%s\" \"%s\" /Y"
+                incdir = join("%$(ConfigurationName)", "include")
+            else
+                error(_ACTION .. " is unsupported, patch me :-)")
+            end
+
+            -- paths here may contain stuff like $(macro) which breaks 
+            -- incdir may contain $(stuff), which breaks premakes path.join(),
+            -- so we have got to do it the manual way 8=)
+            incdir = DIST_DIR .. "/" .. incdir
+
+            local cmds = {}
+            local srcdir = sourcedir()
+            for i=1, #headers do
+                local header = path.getrelative(BUILD_DIR,
+                                                join(srcdir, headers[i]))
+                local dest = path.getrelative(BUILD_DIR,
+                                              join(incdir,
+                                              path.getname(header))) 
+                -- unescape %$ back to $
+                dest = string.gsub(dest, "%%%$", "$")
+
+                if string.match(_ACTION, "vs20*") then
+                    -- convert paths for cmd :'(
+
+                    header = string.gsub(header, "/", "\\")
+                    dest = string.gsub(dest, "/", "\\")
+                end
+
+                cmds[i] = string.format(copycmd, header, dest)
+            end
+            prebuildcommands(cmds)
         end
     end
 end
